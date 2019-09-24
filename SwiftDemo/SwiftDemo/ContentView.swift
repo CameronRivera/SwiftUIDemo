215f986c3e20f2c103054cf5ab9c8632696e004d
Cameron Rivera <cameronrivera@Camerons-MacBook-Pro.local>
2019-09-24 15:52:20 -0400

    Added Xcode project

---
+++ SwiftDemo/SwiftDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftDemo
+//
+//  Created by Cameron Rivera on 9/24/19.
+//  Copyright © 2019 Cameron Rivera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
